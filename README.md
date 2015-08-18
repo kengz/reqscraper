abde563963cf039f9f36f69b6160e7665ba0fc46
kengz <kengzwl@gmail.com>
2015-08-18 23:23:22 +0400

    added scrapeCrawl, updated docs

--- README.md
+++ README.md
@@ -10,14 +10,22 @@ Both return promise. `req` has internal control structure to retry request up to
 #### Brief API doc
 - `req(options)`, where `options` is a request options object. See [`requestJS`](https://github.com/request/request) for full detail.
 
-- `scrape(dyn, url, scope, selector)`, where `dyn` is the optional boolean to use dynamic scraping, using `x-ray-phantom`; `url` is the page url, `scope` and `selector` are some HTML selectors. See [`x-ray`](https://github.com/lapwinglabs/x-ray) for full detail.
+- `scrape(dyn, url, scope, selector)`, where `dyn` is the boolean to use dynamic scraping using `x-ray-phantom`; `url` is the page url, `scope` and `selector` are some HTML selectors. See [`x-ray`](https://github.com/lapwinglabs/x-ray) for full detail.
+
+- `scrapeCrawl(dyn, url, selector, tailArr)`, where `dyn` is true for dynamic scraping using `x-ray-phantom`;
+
+#### `req(options)`
+Convenient wrapper for `request js` - HTTP request method that returns a promise.
+
+| param | desc |
+|:---|:---|
+| `options` | A `request` options object. See [`requestJS`](https://github.com/request/request) for full detail. |
 
 
 ```Javascript
 // imports
-var scraper = require(__dirname + '/scraper.js');
+var scraper = require('reqscraper');
 var req = scraper.req; // the request module
-var scrape = scraper.scrape; // the x-ray module
 
 // sample use of req
 var options = {
@@ -33,7 +41,22 @@ var options = {
 return req(options)
 // prints the result
 .then(console.log)
+```
 
+#### `scrape(dyn, url, scope, selector)`
+Scraper that returns a promise. Backed by [`x-ray`](https://github.com/lapwinglabs/x-ray).
+
+| param | desc |
+|:---|:---|
+| `dyn` | the boolean to use dynamic scraping using `x-ray-phantom` |
+| `url` | the page url to scrape |
+| `[scope]` | Optional scope to narrow now the target HTML for selector |
+| `selector` | HTML selector. See [`x-ray`](https://github.com/lapwinglabs/x-ray) for full detail. |
+
+```Javascript
+// imports
+var scraper = require('reqscraper');
+var scrape = scraper.scrape; // the scraper
 
 // sample use of scrape, non-dynamic
 return scrape(false, 'https://www.google.com', 'body')
@@ -45,3 +68,65 @@ return scrape(false, 'https://www.google.com', 'body', ['li'])
 // prints the <li>'s inside the <body> tag
 .then(console.log)
 ```
+
+
+#### `scrapeCrawl(dyn, url, selector, tailArr)`
+An extension of `scrape` above with crawling capability. Returns a promise with results in a tree-like JSON structure. Crawls by a breath-first tree structure, and does not crawl deeper if the root of a branch is not crawlable.
+
+| param | desc |
+|:---|:---|
+| `dyn` | the boolean to use dynamic scraping using `x-ray-phantom` |
+| `url` | the base page url to scrape and crawl from |
+| `selector` | The selector for the base page (first level) |
+| `tailArr` | An array of selectors for each level to crawl. Note that a preceeding selector must specify the urls to crawl via `hrefs`. |
+
+
+```Javascript
+// imports
+var scraper = require('reqscraper');
+var scrapeCrawl = scraper.scrapeCrawl; // the scrape-crawler
+
+// dynamic scraper
+var dc = scrapeCrawl.bind(null, true)
+// static scraper
+var sc = scrapeCrawl.bind(null, false)
+
+// sample use of scrape-crawl, static
+
+// base selector, level 0
+// has attribute `hrefs` for crawling next
+var selector0 = {
+    img: ['.dribbble-img'],
+    h1: ['h1'],
+    hrefs: ['.next_page@href']
+}
+
+// has attribute `hrefs` for crawling
+var selector1 = {
+    h1: ['h1'],
+    hrefs: ['.next_page@href']
+}
+// the last selector where crawling ends; no need for `hrefs`
+var selector2 = {
+    h1: ['h1']
+}
+
+// Sample call of the method
+sc(
+    'https://dribbble.com', 
+    selector0,
+    // crawl for 3 more times before stoppping at the 4th level
+    [selector1, selector1, selector1, selector2]
+    )
+.then(function(res){
+    // prints the result
+    console.log(JSON.stringify(res, null, 2))
+})
+```
+
+
+## Changelog
+
+`Aug 18 2015`
+- Added scrapecrawl, basically a scraper extended from `scrape` that can also crawl.
+- Updated README for better API doc.
--- scraper.js
+++ scraper.js
@@ -7,6 +7,7 @@
 // 3. Both functions return promise for chaining and flow control.
 
 // dependencies
+var _ = require('lodash')
 var q = require('q');
 var request = require('request');
 var Xray = require('x-ray');
@@ -66,8 +67,159 @@ function scrape(dyn, url, scope, selector) {
     return defer.promise;
 }
 
-// exporting HTTP req and scrape
+
+///////////////////////////////////////////
+// Generic scraper extended with crawler //
+///////////////////////////////////////////
+
+// var x = scrape.bind(null, false);
+
+
+// helper: content grabber for xs, signed with the page url
+function grab(url, content) {
+    // make sure hrefs is an array even if is singleton
+    var hrefsAsArray = _.isArray(content.hrefs) ? content.hrefs : [content.hrefs]
+    return {
+        url: url,
+        content: _.omit(content, 'hrefs'),
+        hrefs: hrefsAsArray,
+        child: {}
+    }
+}
+
+// Basically many x's
+// Calls x(url, selector) for each url in urlArr
+// returns a promise of object {url, content}
+function xs(dyn, urlArr, selector) {
+    dyn = dyn || false
+    var x = scrape.bind(null, dyn)
+
+    var promises = []
+    _.each(urlArr, function(url) {
+        var defer = q.defer()
+        promises.push(defer.promise)
+        x(url, selector)
+        .then(grab.bind(null, url))
+        .then(defer.resolve)
+    })
+    return q.all(promises)
+}
+
+// // sample call, non dynamic scraper
+// xs(false, 
+// ['https://www.google.com'], {
+//     res: ['a'],
+//     hrefs: ['a@href']
+// })
+// .then(function(roar){
+//     console.log(roar)
+//     console.log(roar[0].hrefs)
+// })
+
+
+// recursively call xs()
+// extend obj by adding to obj.child the results from urlArr and selector, then recursively crawl from the children using the tailArr
+// where tailArr = array of selectors for the next crawled depths, with the first entry consumed per descend
+function rxs(dyn, obj, urlArr, selector, tailArr) {
+    // console.log('reporting')
+
+    var defer = q.defer()
+
+    // call xs() on all the urlArr, returns promise from grab()
+    xs(dyn, urlArr, selector)
+    .then(function(grabbed){
+        // set child as result from grab()
+        obj.child = grabbed
+
+        var promises = []
+        // recursive part on each child
+        _.map(obj.child, function(o){
+            // console.log("obj child", o)
+            var deferG = q.defer()
+            promises.push(deferG.promise)
+
+            // if x's is callable
+            var expectGChild = !_.isEmpty(tailArr) && !_.isEmpty(o.hrefs)
+            // console.log('expectGChild', expectGChild)
+            if (expectGChild) {
+                var hrefs = o.hrefs
+                var newTailArr = _.clone(tailArr)
+                var newSelector = newTailArr.shift()
+
+                // recursive call
+                rxs(dyn, o, hrefs, newSelector, newTailArr)
+                .then(deferG.resolve)
+                .catch(deferG.resolve)
+            }
+            else {
+                deferG.resolve()
+            }
+        })
+
+        // resolve promise for all child
+        q.all(promises).then(defer.resolve)
+    })
+return defer.promise
+}
+
+
+// scraper that can crawl
+// Note that to crawl, each higher level of selector must have the 'hfres' selector specified
+function scrapeCrawl(dyn, url, selector, tailArr) {
+    var defer = q.defer()
+
+    // the scraped result as object
+    var res = {}
+
+    // the (base) url array
+    var urlArr = _.isArray(url) ? url : [url]
+
+    // call recursie x's and put result thru defer's promise
+    rxs(dyn, res, urlArr, selector, tailArr)
+    .then(defer.resolve.bind(null, res))
+
+    return defer.promise
+}
+
+
+// // sample call
+// // static scrape-crawler
+// var sc = scrapeCrawl.bind(null, false)
+
+// // base selector, level 0
+// // has attribute `hrefs` for crawling next
+// var selector0 = {
+//     img: ['.dribbble-img'],
+//     h1: ['h1'],
+//     hrefs: ['.next_page@href']
+// }
+
+// // has attribute `hrefs` for crawling
+// var selector1 = {
+//     h1: ['h1'],
+//     hrefs: ['.next_page@href']
+// }
+// // the last selector where crawling ends; no need for `hrefs`
+// var selector2 = {
+//     h1: ['h1']
+// }
+
+// // Sample call of the method
+// sc(
+//     'https://dribbble.com', 
+//     selector0,
+//     // crawl for 3 more times before stoppping at the 4th level
+//     [selector1, selector1, selector1, selector2]
+//     )
+// .then(function(res){
+//     // prints the result
+//     console.log(JSON.stringify(res, null, 2))
+// })
+
+
+// exporting HTTP req and scrape, scrapeCrawl
 module.exports = {
     req: req,
-    scrape: scrape
+    scrape: scrape,
+    scrapeCrawl: scrapeCrawl
 }
